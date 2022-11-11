3135051da6e078486fef8bf00a009e4d1754ca9f
Ilker Keklik <ilkerkeklik50@gmail.com>
2022-11-11 22:29:57 +0300

    FC

---
+++ MyJavaGame/application/Tile.java
@@ -0,0 +1,24 @@
+//150120023 YUSUF DUMAN 150120074 ÝLKER KEKLÝK   
+
+package application;
+
+public interface  Tile  {
+
+    public abstract boolean getUp();
+
+    public abstract  boolean getDown();
+
+    public abstract boolean getRight();
+
+    public abstract boolean getLeft();
+
+    public abstract void setUp(boolean up );
+
+    public abstract void setDown(boolean down);
+
+    public abstract void setRight(boolean right);
+
+    public abstract void setLeft(boolean left);
+
+
+}
